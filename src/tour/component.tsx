c208746d35cd7047b7bbbfc3abe6a2a4f825a5fe
Arthurk12 <arthur_bockmann@hotmail.com>
2026-07-10 15:14:35 -0300

    chore: fixes ts and lint errors
    - Fixes the lint command that was triggering lint inside ./src/locales
      folder
    - Fixes multiple code identation issues(mixed spaces and tabs)
    - Add skip lib check to tsconfig
    -

--- src/tour/component.tsx
+++ src/tour/component.tsx
@@ -57,14 +57,13 @@ export function startTour(
     presentationInitiallyOpened,
   ).forEach((feature) => {
     feature.steps.forEach((step) => {
-      /* @ts-ignore */
       tour.addStep({
         ...step,
         // Only show step if the element is visible
         showOn: () => !!document.querySelector(
           step.attachTo.element,
         ),
-      });
+      } as Parameters<typeof tour.addStep>[0]);
     });
   });
 
@@ -84,10 +83,14 @@ function TourPlugin(
     fallbackLocale: 'en',
   });
 
-  const layoutInformation = pluginApi.useUiData(LayoutPresentationAreaUiDataNames.CURRENT_ELEMENT, [{
-    isOpen: presentationInitiallyOpened,
-    currentElement: UiLayouts.WHITEBOARD,
-  }]);
+  const layoutInformation = pluginApi.useUiData(
+    LayoutPresentationAreaUiDataNames.CURRENT_ELEMENT,
+    [{
+      isOpen: presentationInitiallyOpened,
+      currentElement: UiLayouts.WHITEBOARD,
+    },
+    ],
+  );
 
   // TODO revisit when fixed
   // const settings = pluginApi.usePluginSettings()?.data;
@@ -148,9 +151,9 @@ function TourPlugin(
         icon: 'presentation',
         onClick: async () => {
           setPresentationInitiallyOpened(layoutInformation[0]?.isOpen);
-					pluginLogger.info({
-			      logCode: 'plg_started',
-			    }, `Plugin started: ${pluginApi.pluginName}`);
+          pluginLogger.info({
+            logCode: 'plg_started',
+          }, `Plugin started: ${pluginApi.pluginName}`);
           // ensure only userList is open (to also work on Mobile)
           pluginApi.uiCommands.sidekickOptionsContainer.close();
           pluginApi.uiCommands.sidekickOptionsContainer.open();
